5f7e42081473fc40cd101c9d7bf02ec8e513dd6b
alexkoenig21 <78494222+alexkoenig21@users.noreply.github.com>
2021-02-23 21:19:36 -0700

    Update DiscountCalculator.java

--- P4.11/DiscountCalculator.java
+++ P4.11/DiscountCalculator.java
@@ -1,9 +1,15 @@
-import java.util.Scanner;
-
-public class DiscountCalculator
+public class calculator
 {
-   public static void main(String[] args)
-   {
-   
-   }
+	public static void main(String args[]) 
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Enter the number of movie rentals: ");
+		int rental = myScanner.nextInt();
+		System.out.println("Enter the number of members referred to the video club: ");
+		int referral  = myScanner.nextInt();
+		double discount = referral + rental; 
+		System.out.println("The discount is equal to: " + Math.min(discount, 50.0) + "%.");
+
+	}
+	
 }
