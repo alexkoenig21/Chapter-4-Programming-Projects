8453c02a48ce944a59c7119c07c229fca9aa358e
alexkoenig21 <78494222+alexkoenig21@users.noreply.github.com>
2021-02-23 21:20:19 -0700

    Update PhoneNumberConversion.java

--- P4.8/PhoneNumberConversion.java
+++ P4.8/PhoneNumberConversion.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
-
-public class PhoneNumberConversion
+public class pseudocode
 {
-   public static void main(String[] args)
-   {
-
-   }
+	static String pseudocode = "2625991416";
+	public static void main(String args[])
+	{
+		String sub1 = pseudocode.substring(0, 3);
+		String sub2 = pseudocode.substring(3, 6);
+		String sub3 = pseudocode.substring(6);
+		System.out.println("(" + sub1 + ")" + sub2 + "-" + sub3);
+	}
 }
